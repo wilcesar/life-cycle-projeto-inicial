e284dd70c747d5c2baaa59c9146576e5de9d7fd9
Wilian Cesar <wilcesar@live.com>
2025-09-02 20:25:19 -0300

    Implementando alteração do item.

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Item } from 'src/app/interfaces/iItem';
 import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
@@ -6,7 +7,8 @@ import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, OnChanges {
+  @Input() itemQueVaiSerEditado!:  Item;
   valorItem! : string;
   constructor(private listaService: ListaDeCompraService) { }
 
@@ -18,5 +20,8 @@ export class InputComponent implements OnInit {
   }
   limparCampo(){
     this.valorItem = '';
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes['itemQueVaiSerEditado'].currentValue)
   }
 }
