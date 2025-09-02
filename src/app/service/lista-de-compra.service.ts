3d162ef700ff87e00ed5271971590f019bcacbf2
Wilian Cesar <wilcesar@live.com>
2025-09-02 19:58:41 -0300

    Alimentando lista de compras.

--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -34,4 +34,20 @@ export class ListaDeCompraService {
   getListaDeCompra(){
     return this.listaDeCompra;
   }
+  criarItem(nomeDoitem : string)
+  {
+    const id = this.listaDeCompra.length +1;
+    const item : Item = {
+      id: id,
+      nome: nomeDoitem,
+      data: new Date().toLocaleDateString('pt-BR'),
+      comprado: false
+    };
+    return item;
+  }
+
+  adicionarItemNaLista(nomeDoItem : string){
+    const item = this.criarItem(nomeDoItem);
+    this.listaDeCompra.push(item);
+  }
 }
